1e4d5aa7d63590b61ada8f17de0e27c29e22a866
Anthony Drury <anthony.drury1@gmail.com>
2019-09-05 17:19:58 +1000

    feat: add missing imports in modules

--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from "@angular/core";
 import { FormModule } from "../form/form.module";
 import { HomeComponent } from "./home.component";
 import { SmashPlayerActionComponent } from "./smash-player-action/smash-player-action.component";
-import { SmashPlayerWinComponent } from './smash-player-win/smash-player-win.component';
-import { SmashPlayerTotalComponent } from './smash-player-total/smash-player-total.component';
-import { SmashPlayerBiggestComponent } from './smash-player-biggest/smash-player-biggest.component';
+import { SmashPlayerBiggestComponent } from "./smash-player-biggest/smash-player-biggest.component";
+import { SmashPlayerTotalComponent } from "./smash-player-total/smash-player-total.component";
+import { SmashPlayerWinComponent } from "./smash-player-win/smash-player-win.component";
 
 @NgModule({
-  declarations: [HomeComponent, SmashPlayerActionComponent, SmashPlayerWinComponent, SmashPlayerTotalComponent, SmashPlayerBiggestComponent],
+  declarations: [
+    HomeComponent,
+    SmashPlayerActionComponent,
+    SmashPlayerWinComponent,
+    SmashPlayerTotalComponent,
+    SmashPlayerBiggestComponent
+  ],
   imports: [FormModule]
 })
 export class HomeModule {}
